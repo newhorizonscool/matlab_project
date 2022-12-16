4d1153c4816f0a5cd9e5a60639f7ff02a00e5dfd
newhorizonscool <evnevzoroff@yandex.ru>
2022-12-16 16:45:08 +0300

    Bob and Eva

--- Capacity.m
+++ Capacity.m
@@ -1,16 +1,42 @@
 clear all; close all; clc
 N = 1e3;
 raz = 1e3;
-EbNo = 1:20;
-C_mean = zeros(1, 20);
+EbNo_BOB = 1:20;
+C_mean_BOB = zeros(1, 20);
+
+EbNo_EVA = 5:15;
+C_mean_EVA = zeros(1, 11);
+
 for ii = 1:20
     for jj = 1:raz
         H = abs((randn(1) + 1i*randn(1))/sqrt(2)).^2;
-        C(jj) = log2(1+H*EbNo(ii));
+        C_BOB(jj) = log2(1+H*EbNo_BOB(ii));
     end
-    C_mean(ii) = mean(C);
+    C_mean_BOB(ii) = mean(C_BOB);
 end
-plot(EbNo(1:end),C_mean(1:end),'-o')
+figure
+plot(EbNo_BOB(1:end),C_mean_BOB(1:end),'-o')
 
+title('Capacity Alica-Bob')
+xlabel('SNR')
+ylabel('Capacity')
+
+
+
+for ii = 1:11
+    for jj = 1:raz
+        H = abs((randn(1) + 1i*randn(1))/sqrt(2)).^2;
+        C(jj) = log2(1+H*EbNo_EVA(ii));
+    end
+    C_mean_EVA(ii) = mean(C);
+end
+
+figure
+plot(EbNo_EVA(1:end),C_mean_EVA(1:end),'-x')
+
+title('Capacity Alica-Eva')
+xlabel('SNR')
+ylabel('Capacity')
+plot(EbNo_EVA(1:end),C_mean_EVA(1:end),'-x')
 
 
