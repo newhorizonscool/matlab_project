bd036c1e71d613f05ec9c9f7b28386b288265c0a
newhorizonscool <evnevzoroff@yandex.ru>
2022-12-16 16:31:24 +0300

    Capacity_Channel

--- Capacity.m
+++ Capacity.m
@@ -1,15 +1,16 @@
 clear all; close all; clc
 N = 1e3;
-raz = 1e3
+raz = 1e3;
 EbNo = 1:20;
-for ii = 1:length(EbNo):
+C_mean = zeros(1, 20);
+for ii = 1:20
     for jj = 1:raz
-        H = abs(mean((randn(1,N) + 1i*randn(1,N))/sqrt(2))).^2;
-        asd = mean(randn(log2(1+H*EbNo),10000));
-        C(ii) = 
+        H = abs((randn(1) + 1i*randn(1))/sqrt(2)).^2;
+        C(jj) = log2(1+H*EbNo(ii));
     end
+    C_mean(ii) = mean(C);
 end
-plot(EbNo(1:end),C(1:end),'-o')
+plot(EbNo(1:end),C_mean(1:end),'-o')
 
 
 
